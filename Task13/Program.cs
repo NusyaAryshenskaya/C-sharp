a595acf557ccd1e48fdee9de72082c9e8ee85319
«Zhenya» <saltykovazh@mail.ru>
2022-12-26 18:39:07 +0400

    Homework Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число: ");
+int N = int.Parse(ReadLine()!);
+
+if (Math.Abs(N) < 100)
+{
+    WriteLine("В номере третьей цифры нет", N);
+    return; 
+}
+
+int Digit3 = (Math.Abs(N) - (Math.Abs(N) / 1000) * 1000) / 100;
+WriteLine("Третья цифра числа {0} равна: {1}", N, Digit3);
+
+return; 
