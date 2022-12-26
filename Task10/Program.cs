5eea9d07a1ab70e222c156c5712983732e0ef040
«Zhenya» <saltykovazh@mail.ru>
2022-12-26 18:27:23 +0400

    homework Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите трехзначное число: ");
+int N = int.Parse(ReadLine()!);
+
+int Second = (Math.Abs(N) - (Math.Abs(N) / 100) * 100) / 10;
+WriteLine("Вторая цифра = {0}", Second );
