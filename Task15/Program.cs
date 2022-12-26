b821bd9193c9a3baac8a2115051cb92bbde411a5
«Zhenya» <saltykovazh@mail.ru>
2022-12-26 19:00:39 +0400

    Homework Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using static System.Console;
+
+Write("Введите номер дня недели: ");
+int Day = int.Parse(ReadLine()!);
+WriteLine ("Является ли этот день выходным: ");
+
+if (Day < 6 && Day > 0)
+{
+    WriteLine("Нет");
+    return; 
+}
+
+if (Day == 6 || Day == 7)
+{
+    WriteLine("Да");
+    return;
+}
+
+if (Day < 1 || Day > 7)
+{
+    WriteLine("Такого дня недели не существует");
+    return;
+}
