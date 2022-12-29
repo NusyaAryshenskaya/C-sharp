475e7459d8900b80dc83bb402614f58f2d108685
«Zhenya» <saltykovazh@mail.ru>
2022-12-29 13:13:24 +0400

    Task19

--- Homework3/Task19/Program.cs
+++ Homework3/Task19/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число: ");
+
+long number = long.Parse(ReadLine()!);
+long rev = 0;
+long number2 = number;
+while (number2 != 0)
+{
+    rev *= 10;
+    rev += number2 % 10;
+    number2 = number2 / 10;
+}
+Write("Данное число является палиндромом: ");
+Write(rev == number ? "Да" : "Нет");
