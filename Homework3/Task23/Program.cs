be0219d3f704b2840e3498541c6482be63bc4ab1
«Zhenya» <saltykovazh@mail.ru>
2022-12-29 13:54:16 +0400

    Task23

--- Homework3/Task23/Program.cs
+++ Homework3/Task23/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите число N:");
+int N = int.Parse(ReadLine()!);
+
+WriteLine("Числа от 1 до N в третьей степени:");
+
+if (N > 0)
+{
+    int index = 1;
+    while (index <= N)
+    {
+        Write($"{Math.Pow(index, 3)} ");
+        index += 1;
+    }
+    WriteLine(" ");
+}
+else
+{
+    int index = 1;
+    while (index >= N)
+    {
+        Write($"{Math.Pow(index, 3)} ");
+        index -= 1;
+    }
+    WriteLine(" ");
+}
