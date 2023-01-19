f7b88cf0dfb1aed846d129a69911e1feaed3fd51
«Zhenya» <saltykovazh@mail.ru>
2023-01-19 09:58:43 +0400

    git Task25

--- Homework4/Task25/Program.cs
+++ Homework4/Task25/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введи число A: ");
+int A = int.Parse(ReadLine()!);
+
+Write("Введи число B: ");
+int B = int.Parse(ReadLine()!);
+
+WriteLine(exponent_A_to_B(A, B));
+
+
+int exponent_A_to_B (int A, int B)
+{
+    int result = 1;
+    for(int i = 0; i < B; i++){
+        result *= A;
+    }
+
+    return result;
+}
