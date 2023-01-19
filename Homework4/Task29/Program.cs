55bfd10f5f2682d8d74b29e5b9c36cc23abdd985
«Zhenya» <saltykovazh@mail.ru>
2023-01-19 11:55:39 +0400

    git Task29

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Массив случайных целых чисел: ");
+
+void Printarray (){
+    int[] array = new int[8];
+    Random random = new Random();
+
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = random.Next(0,100);
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length-1) WriteLine(array[i]);
+        else Write($"{array[i]}, ");
+    }
+}
+
+Printarray();
