87174c001e925080104159e62c3601f5286ef635
«Zhenya» <saltykovazh@mail.ru>
2023-01-19 10:07:03 +0400

    git Task27

--- Homework4/Task27/Program.cs
+++ Homework4/Task27/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+
+Write("Введи число: ");
+int number = int.Parse(ReadLine()!);
+
+WriteLine(sum(number));
+
+
+int sum (int number)
+{
+    number = Math.Abs(number);
+    int result = 0;
+    
+    while(number > 0)
+    {
+        result += number%10;
+        number /= 10;
+    }
+
+    return result;
+}
