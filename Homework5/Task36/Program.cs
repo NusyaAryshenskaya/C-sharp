cad560428546cd29951329d6a4a2952bef63b6ea
«Zhenya» <saltykovazh@mail.ru>
2023-01-23 15:40:36 +0400

    Git init Task38

--- Homework5/Task36/Program.cs
+++ Homework5/Task36/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int[] array = GetArray(6);
+int sum = SumOddIndex(array);
+WriteLine($"[{String.Join(", ", array)}] -> {sum}");
+
+int [] GetArray(int size)
+{
+    
+    int[] resultArray = new int [size];
+
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = random.Next(-100, 100);
+    }
+
+    return resultArray;
+}
+
+int SumOddIndex(int[] array)
+{
+    int result = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0) result += array[i-1];
+    }
+
+    return result;
+}
