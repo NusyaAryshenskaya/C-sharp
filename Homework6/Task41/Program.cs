934f84393ca76ffe6c6c891389a131769c3f68e3
«Zhenya» <saltykovazh@mail.ru>
2023-01-26 10:31:28 +0400

    Git Task41

--- Homework6/Task41/Program.cs
+++ Homework6/Task41/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите числа через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int countOtherZero = GetCountOtherZero(array);
+WriteLine($"{String.Join(", ", array)} -> {countOtherZero}");
+int GetCountOtherZero(string[] array){
+    int result = 0;
+
+    foreach (string s in array)
+    {
+        if(int.Parse(s) > 0) result++;
+    }
+
+    return result;
+}
