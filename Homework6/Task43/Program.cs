51c266495e1e34a33042a286ce97db9143189fba
«Zhenya» <saltykovazh@mail.ru>
2023-01-26 10:45:02 +0400

    Git Task43

--- Homework6/Task43/Program.cs
+++ Homework6/Task43/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Точка пересечения двух прямых: y = k1 * x + b1, y = k2 * x + b2");
+
+double b1 = CheckInput("Введите b1: ");
+double k1 = CheckInput("Введите k1: ");
+double b2 = CheckInput("Введите b2: ");
+double k2 = CheckInput("Введите k2: ");
+
+if (k1 == k2 && b1 == b2)
+{
+    WriteLine("ЛПрямые одинаковые (сливаются в одну)");
+}
+else if (k1 == k2)
+{
+    WriteLine("Прямые не пересекаются, они паралельные");
+}
+else
+{
+    double x = (b2-b1) / (k1 - k2);
+    double y = (k1 * (b2-b1))/ (k1-k2) + b1;
+    WriteLine($"Точка пересечения: x={x}, y={y}");
+}
+
+
+double CheckInput(string message)
+{
+    Write(message);
+    if (!double.TryParse(ReadLine(), out double result))
+    {
+        Environment.Exit(0);
+    }
+
+    return result;
+
+}
