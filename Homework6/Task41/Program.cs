6809147bb0d06eaa4c88054ee1f5180b96fdb525
«Zhenya» <saltykovazh@mail.ru>
2023-01-26 10:47:37 +0400

    Git Task41

--- Homework6/Task41/Program.cs
+++ Homework6/Task41/Program.cs
@@ -3,9 +3,9 @@ Clear();
 
 Write("Введите числа через пробел: ");
 string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-int countOtherZero = GetCountOtherZero(array);
-WriteLine($"{String.Join(", ", array)} -> {countOtherZero}");
-int GetCountOtherZero(string[] array){
+int count = GetCount(array);
+WriteLine($"{String.Join(", ", array)} -> {count}");
+int GetCount(string[] array){
     int result = 0;
 
     foreach (string s in array)
