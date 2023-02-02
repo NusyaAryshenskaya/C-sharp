28370148eee07f09bed00611113fd1c230ca4df3
«Zhenya» <saltykovazh@mail.ru>
2023-02-02 13:58:29 +0400

    Git Task64

---
+++ Homework9/Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введите N: ");
+int N = int.Parse(ReadLine()!);
+PrintNumbers(N, 1);
+
+void PrintNumbers(int num1, int num2)
+{
+    if (num1 == num2) Write(num1);
+    else
+    {
+        PrintNumbers(num1, num2 + 1);
+        Write($",{num2}");
+    }
+}
