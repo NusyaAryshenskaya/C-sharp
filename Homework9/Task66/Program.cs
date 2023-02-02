07cf603a70c1f15b36db80315c309f670f1c7639
«Zhenya» <saltykovazh@mail.ru>
2023-02-02 14:18:01 +0400

    Git Task66

---
+++ Homework9/Task66/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите значения двух элементов через пробел: ");
+int[] num = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+WriteLine("Сумма натуральных элементов в промежутке заданных чисел: ");
+WriteLine(SumNumber(num[0], num[1]));
+
+
+int SumNumber(int num1, int num2)
+{
+    return (num1 < num2) ?(num1 == num2 ? num1 : SumNumber(num1, num2 - 1) + num2) 
+    : (num1 == num2 ? num1 : SumNumber(num1, num2 + 1) + num2);
+}
