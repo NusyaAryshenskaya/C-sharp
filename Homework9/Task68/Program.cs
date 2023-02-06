2c2315f33959f1fde41105b02eea6423f0dc03cf
«Zhenya» <saltykovazh@mail.ru>
2023-02-06 15:01:54 +0400

    Git Task68

---
+++ Homework9/Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введи M и N для вычяслениия функции Аккермана через пробел: ");
+
+ulong[] parametr = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), ulong.Parse);
+
+WriteLine($"A({parametr[0]},{parametr[1]}) = {Akkerman(parametr[0], parametr[1])}");
+
+ulong Akkerman(ulong numM, ulong numN)
+{
+    if (numM == 0) return numN + 1;
+    if (numM > 0 && numN == 0)
+    {
+        return Akkerman(numM - 1, 1);
+    }
+    if (numM > 0 && numN > 0)
+    {
+        return Akkerman(numM - 1, Akkerman(numM, numN - 1));
+    }
+    return 0;
+}
